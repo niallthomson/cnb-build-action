bb30b41a49426e5ad7d94918e24eba4a678430dd
Niall Thomson <thomson.niall@gmail.com>
2020-04-11 14:52:00 -0700

    Added ability to authenticate with registry for push

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,6 +16,10 @@ if [ "${INPUT_SNAPSHOT}" == "true" ]; then
   INPUT_IMAGENAME="$INPUT_IMAGENAME:$sha_tag"
 fi
 
+if [ -n "${INPUT_REGISTRY}" ]; then
+  echo ${INPUT_REGISTRYPASSWORD} | docker login -u ${INPUT_REGISTRYUSERNAME} --password-stdin ${INPUT_REGISTRY}
+fi
+
 pack build $INPUT_IMAGENAME -p /github/workspace/$INPUT_PATH --builder $INPUT_BUILDER $args
 
 echo "::set-output name=imageName::$INPUT_IMAGENAME"
